1af89656ee3f0b705ad43c4d8820324fe1a9d0c3
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2025-05-06 20:03:41 +0600

    learned group, scale, positioning, camera helper, axes helper and viewing in 3d perspective

--- script.js
+++ script.js
@@ -7,12 +7,25 @@ console.log(THREE);
 // Scene
 const scene = new THREE.Scene();
 
+// Group
+const group = new THREE.Group();
+
 // Mesh 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 'purple' });
 const mesh = new THREE.Mesh(geometry, material);
+mesh.position.z = 1;
+mesh.scale.x = 1;
 
-scene.add(mesh);
+// Mesh2
+const geometryT = new THREE.BoxGeometry(1, 1, 1);
+const materialT = new THREE.MeshBasicMaterial({ color: "green" });
+const meshT = new THREE.Mesh(geometryT, materialT);
+meshT.position.y = 2;
+
+group.add(mesh, meshT);
+group.position.x = 1;
+scene.add(group);
 
 // Camera 
 const aspect = {
@@ -21,9 +34,13 @@ const aspect = {
 };
 
 const camera = new THREE.PerspectiveCamera(75, aspect.width / aspect.height);
-camera.position.z = 3;
 camera.position.x = 1;
 camera.position.y = 1;
+camera.position.z = 5;
+// const helper = new THREE.CameraHelper(camera);
+// scene.add(helper);
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
 scene.add(camera);
 
 // Renderer
